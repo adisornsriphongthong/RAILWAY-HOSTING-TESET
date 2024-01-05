449c8658e8156efc4a79f9535d88a0b05a2a9294
adisorn <adisornsriphongthong@gmail.com>
2024-01-05 16:09:01 +0700

    added

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 const expres = require('express')
 const app = expres()
 require('dotenv').config()
+const port = process.env.port
 
 app.get('/', (req, res) => {
     res.json({
-        text: process.env.text
+        'hello':'world'
     })
 })
 
-const port = 3000;
 app.listen(port, (err) => {
     if(err) throw err;
     console.log('The server is running on port ' + port)
